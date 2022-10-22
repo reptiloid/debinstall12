b37e9765a22149c72dd55cd9c783b7fce56be441
Kirill <2spybot@gmail.com>
2022-10-22 19:27:53 +0300

    qtile added

---
+++ .config/qtile/scripts/wallpaper.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -o nounset  # exit on uninitialised variable
+set -o errexit  # exit on error
+#set -o xtrace   # debug mode
+setxkbmap -option grp:ralt_rshift_toggle us,ru
+
+picom --transparent-clipping &
+feh --bg-scale ~/Walls/xp.jpg &
