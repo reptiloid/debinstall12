501ec3548ddd167b2500755acd3f05229ee65dad
Slava Nibiru <2spybot@gmail.com>
2023-06-14 19:15:56 +0000

    install apt packages

--- .local/debinstall/pkg_install.sh
+++ .local/debinstall/pkg_install.sh
@@ -15,7 +15,7 @@ install_pkgs() {
 	printf '%s\n' "Installing packages" && sleep 1
 	sudo nala install -y net-tools feh unzip ufw exa \
 		ripgrep fzf autojump bash-completion tar \
-		fonts-dejavu-extra \
+		fonts-dejavu-extra thunar \
         libx11-dev libxft-dev libharfbuzz-dev \
         xinit xserver-xorg x11-xserver-utils xdg-user-dirs \
         libpangocairo-1.0-0 python3-pip python3-xcffib python3-cairocffi
@@ -54,19 +54,6 @@ install_nerdfonts(){
     fc-cache
 }
 
-deploy_dotfiles() {
-	echo
-	printf '%s\n' "Deploying dotfiles" && sleep 1
-	for file in dunst mpv mutt newsboat picom sxhkd vifm ; do cp -r "$SRC_DIR"/"$DOT_DIR"/.config/"$file" ~/.config/ ; done
-	for file in chwall-dmenu mpvload netcon poweroffreboot statusbar.sh usbmount usbunmount usbpoweroff ; do cp -r "$SRC_DIR"/"$DOT_DIR"/.local/bin ~/.local/ ; done
-	mkdir -p ~/.local/share
-	cp -r "$SRC_DIR"/"$DOT_DIR"/.local/share/fonts ~/.local/share/
-	mkdir -p ~/Pictures	# In case xdg-user-dirs-update failed for some reason
-	cp -r "$SRC_DIR"/"$DOT_DIR"/Pictures/Wallpapers ~/Pictures/
-	touch ~/Pictures/defwall.jpg
-	ln -sf ~/Pictures/Wallpapers/wallpaper0002.jpg ~/Pictures/defwall.jpg
-	for file in .vimrc .xinitrc .Xresources ; do cp -r "$SRC_DIR"/"$DOT_DIR"/"$file" ~/ ; done
-}
 
 install_qtile() {
 	echo
@@ -100,13 +87,9 @@ main() {
 	install_qtile12
     get_sl_tools
 	install_st
-
-	# get_dotfiles
-	# deploy_dotfiles
 	
 	# enable_firewall
 	
-	# set_permissions
 	# finish
 }
 
