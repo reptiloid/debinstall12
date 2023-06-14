54d78565c65afc10d1590c886a8e8d07fa6dc383
Slava Nibiru <2spybot@gmail.com>
2023-06-14 19:06:04 +0000

    nixpkg.sh edited online with Bitbucket

--- .local/debinstall/nixpkg.sh
+++ .local/debinstall/nixpkg.sh
@@ -11,9 +11,11 @@ install_nix_pkgs() {
 	echo
 	printf '%s\n' "Installing Nix Pkgs" && sleep 1
 	nix-env -iA nixpkgs.picom nixpkgs.htop-vim \
+	    nixpkgs.lf nixpkgs.syncthing \
+		nixpkgs.obsidian nixpkgs.freetube \
 		nixpkgs.neovim \
 		nixpkgs.starship \
-        # nixpkgs.obsidian nixpkgs.librewolf
+        nixpkgs.librewolf
 }
 
 main() {
