2b675cb1c1051d5a6b097be801bacc03547ca5b9
reptiloid <2spybot@gmail.com>
2023-06-17 16:17:28 +0000

    Update python_install.sh

--- .local/debinstall/python_install.sh
+++ .local/debinstall/python_install.sh
@@ -1,7 +1,7 @@
 install_pyenv_dependencies() {
-    sudo nala install -y make build-essential libssl-dev zlib1g-dev \
-        libbz2-dev libreadline-dev libsqlite3-dev llvm libncurses5-dev \
-        libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl
+	sudo nala install -y make build-essential libssl-dev zlib1g-dev \
+		libbz2-dev libreadline-dev libsqlite3-dev llvm libncurses5-dev \
+		libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python3-openssl
 }
 
 install_pyenv() {
@@ -10,21 +10,21 @@ install_pyenv() {
 
 set_bashrc_pyenv() {
 	echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-    	echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-    	echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+	echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+	echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 }
 
 set_profile_pyenv() {
 	echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
-    	echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
-    	echo 'eval "$(pyenv init -)"' >> ~/.profile
+	echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
+	echo 'eval "$(pyenv init -)"' >> ~/.profile
 }
 
 main() {
 	install_pyenv_dependencies
-   	install_pyenv
-    	set_bashrc_pyenv
-    	set_profile_pyenv
+	install_pyenv
+	set_bashrc_pyenv
+	set_profile_pyenv
 }
 
 main   
