a7a6a01fa781452592ce47a996dbceab7308ca1e
reptiloid <2spybot@gmail.com>
2023-06-18 16:39:06 +0000

    Update appimg_manager.sh

--- .local/debinstall/appimg_manager.sh
+++ .local/debinstall/appimg_manager.sh
@@ -6,18 +6,23 @@ clone_am() {
 	echo
 	printf '%s\n' "Getting AM-application-manager.git" && sleep 1
 	cd "$SRC_DIR" || exit
-  git clone https://github.com/ivan-hc/AM-application-manager.git
+	git clone https://github.com/ivan-hc/AM-application-manager.git
 }
 
 install_am() {
 	cd "$SRC_DIR"/AM-application-manager || exit
-  chmod a+x INSTALL
-  sudo ./INSTALL
+	chmod a+x INSTALL
+	sudo ./INSTALL
+}
+
+install_pkgs() {
+	am -i neovim chromium
 }
 
 main() {
-  clone_am
-  install_am
+	clone_am
+	install_am
+	install_pkgs
 }
 
 main
