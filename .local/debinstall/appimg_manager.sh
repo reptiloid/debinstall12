bd94e6a4b1146c8cbb5c9a5d0188a46b2d821d26
reptiloid <2spybot@gmail.com>
2023-06-18 16:28:51 +0000

    Create appimg_manager.sh

---
+++ .local/debinstall/appimg_manager.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+SRC_DIR=~/.local/debinstall/src
+
+clone_am() {
+	echo
+	printf '%s\n' "Getting AM-application-manager.git" && sleep 1
+	cd "$SRC_DIR" || exit
+  git clone https://github.com/ivan-hc/AM-application-manager.git
+}
+
+install_am() {
+	cd "$SRC_DIR"/AM-application-manager || exit
+  chmod a+x INSTALL
+  sudo ./INSTALL
+}
+
+main() {
+  clone_am
+  install_am
+}
+
+main
