1e33bf689abfe579247143869a941957c3cbd885
reptiloid <2spybot@gmail.com>
2023-06-18 02:57:08 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,6 +12,9 @@ source "$INS_DIR"/nvim_configs.sh
 # Install PyEnv
 source "$INS_DIR"/python_install.sh
 
+# Set Permissions
+source "INS_DIR"/set_permissions.sh
+
 # 72726LV-7JFW5TZ-AMAEXUH-MBREDU7-X52GVUK-BBZJKP6-IDTPQY6-KTZQZAF
 
 sudo apt autoremove
