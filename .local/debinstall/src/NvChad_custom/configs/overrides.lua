800a07e91ec4197976a512d6fbd380739ae4648d
reptiloid <2spybot@gmail.com>
2023-06-19 13:10:44 +0000

    Update overrides.lua

--- .local/debinstall/src/NvChad_custom/configs/overrides.lua
+++ .local/debinstall/src/NvChad_custom/configs/overrides.lua
@@ -4,14 +4,7 @@ M.treesitter = {
   ensure_installed = {
     "vim",
     "lua",
-    "html",
-    "css",
-    "javascript",
-    "typescript",
-    "tsx",
-    "c",
-    "markdown",
-    "markdown_inline",
+    "python",
   },
   indent = {
     enable = true,
@@ -26,17 +19,6 @@ M.mason = {
     -- lua stuff
     "lua-language-server",
     "stylua",
-
-    -- web dev stuff
-    "css-lsp",
-    "html-lsp",
-    "typescript-language-server",
-    "deno",
-    "prettier",
-
-    -- c/cpp stuff
-    "clangd",
-    "clang-format",
   },
 }
 
