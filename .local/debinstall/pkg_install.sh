16a0749265eeb2681ff183444d9e76cb1618cfb6
Kirill <rep@debian>
2023-10-14 20:28:45 +0300

    message

--- .local/debinstall/pkg_install.sh
+++ .local/debinstall/pkg_install.sh
@@ -22,14 +22,18 @@ install_librewolf_bookworm() {
 	sudo nala install -y librewolf
 }
 
+install_additional_pkgs() {
+	echo
+	printf '%s\n' "Installing packages" && sleep 1
+	sudo nala install -y dmenu zsync
+}
 
 install_pkgs() {
 	echo
 	printf '%s\n' "Installing packages" && sleep 1
 	sudo nala install -y net-tools feh unzip ufw exa \
 		ripgrep fzf autojump bash-completion tar \
-    fonts-dejavu-extra dmenu rofi \
-		zsync \
+    fonts-dejavu-extra rofi \
 		libx11-dev libxft-dev libharfbuzz-dev \
 		xinit xserver-xorg x11-xserver-utils xdg-user-dirs \
 		libpangocairo-1.0-0 python3-pip python3-xcffib python3-cairocffi
@@ -112,6 +116,7 @@ enable_firewall() {
 main() {
 	install_nala
 	install_pkgs
+  # install_additional_pkgs
 
   install_nerdfonts_tarxz
 	# install_nerdfonts_zip
